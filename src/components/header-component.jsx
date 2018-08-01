cd0ca2fecaf0de6c1e114b14a6d85f7fabe1dc0f
Fernando Asulay <fernando.aslay@globant.com>
2018-08-01 14:56:52 -0300

    Added bootstrap 4

--- src/components/header-component.jsx
+++ src/components/header-component.jsx
@@ -4,7 +4,19 @@ class HeaderComponent extends Component{
   render(){
     return(
       <div id="header-component-component">
-        <h1>HEADER</h1>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
--- src/pages/home-page/home-page-component.jsx
+++ src/pages/home-page/home-page-component.jsx
@@ -4,7 +4,13 @@ class HomePageComponent extends Component{
   render(){
     return(
       <div id="home-page-component">
-        <h1>HOME PAGE CONTENT</h1>
+        <div class="jumbotron">
+          <h1 class="display-4">Artic Manager Web!</h1>
+          <p class="lead">A simple tool for managing articles with categories and login.</p>
+          <hr class="my-4" />
+          <p>It uses a NodeJS back-end with mongoDb for data storage.</p>
+          <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </div>
       </div>
     );
   }
